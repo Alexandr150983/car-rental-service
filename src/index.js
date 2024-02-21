e78aac9bdb244b60389e0d1929ece64fe6c4aac5
Alexandr150983 <alexandr150983@gmail.com>
2024-02-21 22:49:28 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor} loading={null}>
-      <BrowserRouter>
+      <BrowserRouter basename="/car-rental-service">
         <App />
       </BrowserRouter>
     </PersistGate>
