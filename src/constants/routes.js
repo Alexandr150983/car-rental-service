4739b19ce83d001ab3e1127ad895a305a7c090d5
Alexandr150983 <alexandr150983@gmail.com>
2024-02-21 22:37:34 +0200

    Updates

--- src/constants/routes.js
+++
@@ -1,3 +0,0 @@
-export const HOME = "/";
-export const CATALOG = "/catalog";
-export const FAVORITES = "/favorites";
--- src/redux/Cars/selectors.js
+++
@@ -1,9 +0,0 @@
-export const selectAllCars = (state) => state.cars.items;
-
-export const selectCarsLoading = (state) => state.cars.loading;
-
-export const selectCarsError = (state) => state.cars.error;
-
-export const selectFavorites = (state) => state.favorites.favorites;
-
-
