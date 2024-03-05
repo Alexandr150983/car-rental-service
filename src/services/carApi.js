3ec0b1540c0e50ec0abdad972ffb0480f976f804
Alexandr150983 <alexandr150983@gmail.com>
2024-03-05 21:05:12 +0200

    fix carApi

--- src/services/carApi.js
+++ src/services/carApi.js
@@ -1,18 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// export const fetchCars = createAsyncThunk(
-//   "cars/fetchCars",
-//   async ({ page = 1, limit = 12 }) => {
-//     let query = `page=${page}&limit=${limit}`;
-
-//     const response = await axios.get(
-//       `https://65cc753edd519126b83e8072.mockapi.io/adverts?${query}`
-//     );
-//     console.log(response.data);
-//     return response.data;
-//   }
-// );
 export const fetchCars = createAsyncThunk("cars/fetchAllCars", async () => {
   const response = await axios.get(
     `https://65cc753edd519126b83e8072.mockapi.io/adverts`
